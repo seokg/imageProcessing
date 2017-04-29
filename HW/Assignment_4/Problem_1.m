ee63c36b32e611c6e479ca7eb95eba9c322d69d9
Edward Seo <seokg1023@gmail.com>
2017-04-29 14:47:23 +0900

    Add files via upload

--- HW/Assignment_4/Problem_1.m
+++ HW/Assignment_4/Problem_1.m
@@ -1,4 +1,4 @@
-function  [output_image, output_image2] = Problem_1()
+function Problem_1()
     % template for ee535 DIP
     % Insert the cod in the designated area below
     
@@ -8,36 +8,42 @@ function  [output_image, output_image2] = Problem_1()
     gray_image = fread(file,fliplr([512,512]),'*uint8')';
     fclose(file);
     
-    %% Sample image for test
-    centerImg = uint8(ones(32,32)*255);
-    padRight = uint8(zeros(256,112));
-    padTop = uint8(zeros(112,32));
-    midImg = [padTop;centerImg;padTop];
-    finalImg = [padRight midImg padRight];    
+%     %% Sample image for test
+%     % sample image test
+%     centerImg = uint8(ones(32,32)*255);
+%     padRight = uint8(zeros(256,112));
+%     padTop = uint8(zeros(112,32));
+%     midImg = [padTop;centerImg;padTop];
+%     finalImg = [padRight midImg padRight];    
+%     
+%     % test
+%     sampleOutput = dft2D(finalImg);
+%     dftMatrix = shiftResult(sampleOutput);
+%     figure; imshow (finalImg,[]); title('orignal');
+%     figure; imshow (log(1+abs(dftMatrix)),[]); title('phase');
+%     figure; imshow ((angle(dftMatrix)),[]); title('angle');
     
-    sampleOutput = fft2(finalImg);
-    sampleOutput2 = dft2D(finalImg);
-    
-    figure; imshow (finalImg,[]); title('orignal');
-    figure; imshow (log(1+abs(sampleOutput)),[]); title('matlab');
-    figure; imshow (log(1+abs(sampleOutput2)),[]); title('test');
     %% -------------- insert code below --------------%%
-    output_image = dft2D(gray_image); % sample code - delete
-    output_image2 = fft2(gray_image);
+    output_image = dft2D(gray_image);
+    tempOutput = shiftResult(output_image);
     
-    figure; imshow (output_image,[]); title('Problem_1.1');
-    figure; imshow (output_image2,[]); title('Problem_1.1');
+    magnitude = log(1+abs(tempOutput));
+    phase = log(1+angle(tempOutput));
+    figure; imshow (magnitude,[]); title('Problem 1.1 a - FFT magnitude');
+    figure; imshow (phase,[]); title('Problem 1.1 a - FFT phase');
     
-
-    % abs for magnitude
-    % angle for phase
     
-    % my code and fft2 is different
-    % first do the ifft and compare the reconstructed image for wrong
-    % algorithm
+    reconImage = invDft2D(output_image);
+    figure; imshow(reconImage,[]); title('Problem1.1 b - reconstrected image')
     
-
-
+    
+    dctImage = dct2D(gray_image);
+%     dctImage2 = ourDCT2D(gray_image);
+    magnitude = log(1+abs(dctImage));
+%     magnitude2 = log(1+abs(dctImage2));
+    figure; imshow(magnitude,[]); title('Problem1.1 b - dctImage')
+%     figure; imshow(magnitude2,[]); title('Problem1.1 b - dctImage2')
+    
 end
 
 function output = dft1D(input)
@@ -53,6 +59,7 @@ function output = dft1D(input)
         idxEven = 1;
         odd = zeros(1, col/2);
         even = zeros(1,col/2);
+
         for i = 1 : col
             checkOddEven = mod(i,2);
             if (checkOddEven==1)
@@ -63,8 +70,8 @@ function output = dft1D(input)
                 idxEven = idxEven + 1;
             end 
         end
-        evenOut = dft1D(odd);
-        oddOut = dft1D(even);
+        evenOut = dft1D(odd); % start with 0 in equation 
+        oddOut = dft1D(even); % start with 1 in equation
         
         for j = 1: col/2
            expVal = - 1i * 2 * pi * (j-1) / col;
@@ -81,13 +88,140 @@ function  output = dft2D(input)
     [row, col] = size(input);
     output = zeros(row,col);
     
+
     for i = 1 : row
         output(i,:) = dft1D(input(i,:));
+    end  
+    
+    for j = 1: col
+       output(:,j) = dft1D(output(:,j).');
+    end
+
+
+end
+
+function output = invDft1D(input)
+
+      %% functions code
+    [~ , col] = size(input);
+    output = zeros(1,col);
+    if(col==1)
+       output = input(col) ;
+   
+    
+    else
+        idxOdd = 1;
+        idxEven = 1;
+        odd = zeros(1, col/2);
+        even = zeros(1,col/2);
+
+        for i = 1 : col
+            checkOddEven = mod(i,2);
+            if (checkOddEven==1)
+                odd(idxOdd) = input(i);
+                idxOdd = idxOdd + 1;
+            else
+                even(idxEven) = input(i);
+                idxEven = idxEven + 1;
+            end 
+        end
+        evenOut = invDft1D(odd);
+        oddOut = invDft1D(even);
+        
+        for j = 1: col/2
+           expVal =  1i * 2 * pi * (j-1) / col;
+           output(j)= evenOut(j) + oddOut(j) * exp (expVal);
+           output(j + col/2) = evenOut(j) - oddOut(j) * exp (expVal);
+           
+        end
+        
+    end
+end
+
+function output = invDft2D(input)
+
+    [row, col] = size(input);
+    output = zeros(row,col);
+    
+    for i = 1 : row
+        output(i,:) = invDft1D(input(i,:));
     end
     
     for j = 1: col
-       output(:,j) = dft1D(input(:,j)');
+       output(:,j) = invDft1D(output(:,j).');
     end
+    
+    output = 1/(row*col) * output;
+end
 
+function output = shiftResult(input)
+    [row, col] = size(input);
+    temp = zeros(row,col);
+    for i = 1 : row
+        idxRow = i + row/2 ;
+        if (idxRow>row)
+            idxRow = i - row/2;
+        end
+        
+        for j = 1: col
+            idxCol = j + col/2;
+            if (idxCol>col)
+                idxCol = j - col/2;
+            end
+            
+            temp(idxRow, idxCol) = input(i,j);
+            
+            
+        end
+    end
+    
+    output = temp;
+    
 
+end
+
+function output = dct1D(input)
+     %% functions code
+    [~ , col] = size(input);
+    output = zeros(1,col);
+    
+    
+    
+    
+    y = [input(1:2:col) input(col:-2:2) ];
+    dftValue = dft1D(y);
+    for j = 1: col
+        expVal = -1i*j*pi/(2*col);
+        if (j ==1)
+            output(j) = sqrt(1/(4*col))*dftValue(j) * exp(expVal)*2;
+        else
+            output(j) = sqrt(1/(2*col))*dftValue(j) * exp(expVal)*2;
+        end
+    end    
+end
+
+function output = dct2D(input)
+    [row, col] = size(input);
+    output = zeros(row,col);
+    
+    for i = 1 : row
+        output(i,:) = dct1D(input(i,:));
+%         output(i,:) = dct(double(input(i,:)));
+        
+    end
+    
+    for j = 1: col
+       output(:,j) = dct1D(output(:,j).');
+%        output(:,j) = dct(double(output(:,j).'));
+
+    end
+end
+
+function [X]=ourDCT(x)
+
+N=length(x);
+k=0:(N-1);
+n=0:(N-1);
+
+X=2*double(x)*cos(pi/N*(n'+1/2)*k);
 end
