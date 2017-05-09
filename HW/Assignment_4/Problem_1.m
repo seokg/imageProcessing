56c3578cab4482d48d405e8dc9ccc2875c4cbf83
Edward Seo <seokg1023@gmail.com>
2017-05-09 23:30:34 +0900

    Add files via upload

--- HW/Assignment_4/Problem_1.m
+++ HW/Assignment_4/Problem_1.m
@@ -1,58 +1,460 @@
-function Problem_1()
-%% image Restoration
-%    blur lenna image
-%    add random gaussian noise -> SNR be 12dB
-%    restore the blur image 
-%    
-%    H:LPF corresponding to Hamming Window in the freq domain
-%    implement following restoration filters
-%    calc PSNR
-
-    close all;
-    clear all;
+function [blocDctImage, reconBlocDCTImage]=Problem_1()
+    % template for ee535 DIP
+    % Insert the cod in the designated area below
     
-    imgdir = uigetdir('Image Directory');
-    file = fopen(fullfile(imgdir,'\lenna_gray_256x256.raw'),'rb');
-    grayLenna = fread(file,fliplr([256,256]),'*uint8')';
+    %% loading directory for image files
+    imgdir= uigetdir('Image Directory');
+    file = fopen(fullfile(imgdir,'\Gray_baby_512x512.raw'),'rb');
+    gray_image = fread(file,fliplr([512,512]),'*uint8')';
     fclose(file);
 
-%%  1.1 
+    
+    %% -------------- insert code below --------------%%
+%     output_image = dft2D(gray_image);
+%     tempOutput = shiftResult(output_image);
+%     
+%     magnitude = log(1+abs(tempOutput));
+%     phase = log(1+angle(tempOutput));
+%     figure; imshow (magnitude,[]); title('Problem 1.1 a - FFT magnitude');
+%     figure; imshow (phase,[]); title('Problem 1.1 a - FFT phase');
+%     
+%     
+%     reconImage = invDft2D(output_image);
+%     reconImage2 = ifft2(output_image);
+%     figure; imshow(reconImage,[]); title('Problem1.1 b - reconstrected image')
+%     
+%     
+%     dctImage = dct2D(gray_image);
+%     magnitude = log(1+abs(dctImage));
+%     figure; imshow(magnitude,[]); title('Problem1.2 a - dctmagnitude')
+%      
+%     
+%     reconDCTImage = invDct2D(dctImage);
+%     figure; imshow(reconDCTImage,[]); title('Problem1.2 a - dct reconstructed Image')
+%     
+%     blocDctImage = blocDct2D(gray_image,16);
+%     magnitude = log(1+abs(blocDctImage));
+%     figure; imshow(magnitude,[]); title('Problem1.2 b - block dctmagnitude')
+%     
+%     reconBlocDCTImage = blocInvDct2D(blocDctImage,16);
+%     figure; imshow(reconBlocDCTImage,[]); title('Problem1.2 a - block dct reconstructed Image')
+    
+     dftImage = dht2D(double(gray_image));
+     magnitude = log(1+abs(dftImage));
+    figure; imshow(magnitude*255,[]); title('Problem1.3  - DHT: magnitude')
+    reconDhtImage= invDht2D(dftImage);
+    figure; imshow(reconDhtImage,[]); title('Problem1.3  - DHT: reconstructed Image')
+    
+     [downLow, downHigh] = dwt1D(gray_image, 'horizontal');
+     [W1, W2] = dwt1D(downLow, 'vertical');
+     [W3, W4] = dwt1D(downHigh, 'vertical');
+    figure; imshow([W1 W2; W3 W4],[]); title('Problem1.3  - DHT: magnitude')
 
-% % hamming window / blur image
-% % Outer Product:
-% % --------------
-% w1 = hamming(15);
-% w2 = w1(:) * w1(:).';
+    
+    
+end
+%% DFT
+function output = dft1D(input)
+    %% functions code
+    [~ , col] = size(input);
+    output = zeros(1,col);
+    if(col==1)
+       output = input(col) ;
+    else
+        idxOdd = 1;
+        idxEven = 1;
+        odd = zeros(1, col/2);
+        even = zeros(1,col/2);
 
-% % Rotational
-% % ----------
-% w1 = hamming(15);
-% [x,y] = meshgrid(-7:7);
-% r = sqrt(x.^2 + y.^2);
-% w = zeros(size(r));
-% w(r<=7) = interp1(linspace(-7,7,15),w1,r(r<=7));
-
-% adding gaussian nosie
-%     matlabNosie = imnoise(grayLenna,'gaussian');
-%     figure; imshow(matlabNosie,[]); title('matlab noise');
-    noiseLenna = uint8(gausNoise(double(grayLenna)/255)*255);
-    figure; imshow(noiseLenna,[]); title('Problem 1.1');    
+        for i = 1 : col
+            checkOddEven = mod(i,2);
+            if (checkOddEven==1)
+                odd(idxOdd) = input(i);
+                idxOdd = idxOdd + 1;
+            else
+                even(idxEven) = input(i);
+                idxEven = idxEven + 1;
+            end 
+        end
+        evenOut = dft1D(odd); % start with 0 in equation 
+        oddOut = dft1D(even); % start with 1 in equation
+        
+        for j = 1: col/2
+           expVal = - 1i * 2 * pi * (j-1) / col;
+           output(j)= evenOut(j) + oddOut(j) * exp (expVal);
+           output(j + col/2) = evenOut(j) - oddOut(j) * exp (expVal);
+           
+        end
+        
+    end
 
 end
 
-function output = gausNoise(input)
-    mean = 0;     % default mean
-    var = 0.01;  % default variance
-  
-    output = input + sqrt(var)*randn(size(input)) + mean;
+function  output = dft2D(input)
+    [row, col] = size(input);
+    output = zeros(row,col);
+    
+
+    for i = 1 : row
+        output(i,:) = dft1D(input(i,:));
+    end  
+    
+    for j = 1: col
+       output(:,j) = dft1D(output(:,j).');
+    end
+
+
 end
 
-function output = hammingWindow(inputSize)
-    w = size(inputSize,1);
-    for idx = 1 : inputSize
-        w(idx) = 0.54 - 0.46*cos(2 * pi * (idx-1)/(inputSize-1));
+function output = invDft1D(input)
+
+      %% functions code
+    [~ , col] = size(input);
+    output = zeros(1,col);
+    if(col==1)
+       output = input(col) ;
+
+    else
+        idxOdd = 1;
+        idxEven = 1;
+        odd = zeros(1, col/2);
+        even = zeros(1,col/2);
+
+        for i = 1 : col
+            checkOddEven = mod(i,2);
+            if (checkOddEven==1)
+                odd(idxOdd) = input(i);
+                idxOdd = idxOdd + 1;
+            else
+                even(idxEven) = input(i);
+                idxEven = idxEven + 1;
+            end 
+        end
+%         odd = input(2:2:col);
+%         even = input(1:2:col);
+%         evenOut = invDft1D(even);
+%         oddOut = invDft1D(odd);
+        evenOut = invDft1D(odd);
+        oddOut = invDft1D(even);        
+        for j = 1: col/2
+           expVal = 1i * 2 * pi * (j-1) / col;
+           output(j)= evenOut(j) + oddOut(j) * exp (expVal);
+           output(j + col/2) = (evenOut(j) - oddOut(j) * exp (expVal));
+           
+        end
+        output = output/2;
     end
     
-    output = w* w.';
+end
 
+function output = invDft2D(input)
+
+    [row, col] = size(input);
+    output = zeros(row,col);
+    
+    for i = 1 : row
+        output(i,:) = invDft1D(input(i,:));
+    end
+    
+    for j = 1: col
+       output(:,j) = invDft1D(output(:,j).');
+    end
+    
+%     output = 1/(row*col) * output;
+end
+
+function output = shiftResult(input)
+    [row, col] = size(input);
+    temp = zeros(row,col);
+    for i = 1 : row
+        idxRow = i + row/2 ;
+        if (idxRow>row)
+            idxRow = i - row/2;
+        end
+        
+        for j = 1: col
+            idxCol = j + col/2;
+            if (idxCol>col)
+                idxCol = j - col/2;
+            end
+            
+            temp(idxRow, idxCol) = input(i,j);
+            
+            
+        end
+    end
+    
+    output = temp;
+    
+
+end
+%% DCT
+function output = dct1D(input)
+     %% functions code
+     
+
+    [~ , col] = size(input);
+    if col == 1
+        output = input;
+     else   
+    
+        y = [input(1:2:(col)) input(col:-2:2) ];
+        output = dft1D(y);
+        
+        w = zeros(1,col);
+        for idx = 1: col
+            expVal = -pi*1i*(idx-1)/ (2*col);
+            w(idx) = 2*exp(expVal)/sqrt(2*col);
+%             if idx == 1
+%                 y(idx) = y(idx) * sqrt(2);
+%             end
+        end 
+%           y = sqrt(2*col) * exp(1i*pi*(0:col-1)/(2*col));
+%          y(1) = y(1)/sqrt(2);
+        w(1) = w(1) / sqrt(2);
+        output = w.*output; 
+%         for j = 1: col
+%             realPart = real(output(j));
+%             imgPart = imag(output(j));
+% %             expVal = -1i*(j-1)*pi/(2*col);
+%             if (j ==1)
+%                 output(j) = sqrt(1/col)*(cos(pi*(j-1)/(2*col))*realPart + sin(pi*(j-1)/(2*col))*imgPart);
+% %                 output(j) = sqrt(1/(4*col))*output(j) * exp(expVal)*2;
+%             else
+%                 output(j) = sqrt(2/col)*(cos(pi*(j-1)/(2*col))*realPart + sin(pi*(j-1)/(2*col))*imgPart);
+% %                 output(j) = sqrt(1/(2*col))*output(j) * exp(expVal)*2;
+%             end
+%         end    
+    end
+     output = real(output);
+end
+
+function output = dct2D(input)
+    [row, col] = size(input);
+    output = zeros(row,col);
+    
+    for i = 1 : row
+        output(i,:) = dct1D(input(i,:));
+    end
+    
+    for j = 1: col
+       output(:,j) = dct1D(output(:,j).');
+    end
+end
+
+function output = invDct1D(input)
+    [~, col] = size(input);
+    if (col == 1)
+       output = input; 
+    
+    else
+        y = zeros(1,col);
+        for idx = 1: col
+            expVal = pi*1i*(idx-1)/ (2*col);
+            y(idx) = exp(expVal)*sqrt(2*col);
+%             if idx == 1
+%                 y(idx) = y(idx) * sqrt(2);
+%             end
+        end 
+%           y = sqrt(2*col) * exp(1i*pi*(0:col-1)/(2*col));
+%          y(1) = y(1)/sqrt(2);
+        y(1) = y(1) / sqrt(2);
+        inputVal = y.*input; 
+%         inputRev = zeros(1,col-1);
+%         for j = 2: col
+%             inputRev(col-j+1)=input(j); 
+%         end
+% 
+%         inputTemp = [ input(1), y(2:col).*(input(2:col) - 1i*inputRev) ];
+        dftVal = invDft1D(inputVal); % fft not wrong
+
+%         dftVal = invDft1D(inputTemp); % fft not wrong
+        output = zeros(1,col);
+        output(1:2:col) = dftVal(1:(col/2));
+        output(2:2:col) = dftVal(col:-1:(col/2+1));
+        
+    end
+    
+    output = real(output);
+    
+end
+
+function output = invDct2D(input)
+    [row, col] = size(input);
+    output = zeros(row,col);
+    
+    for i = 1 : row
+        output(i,:) = invDct1D(input(i,:));
+%         output(i,:) = IDCTImpl(input(i,:));
+%          output(i,:) = idct(input(i,:));       
+    end
+    
+    for j = 1: col
+       output(:,j) = invDct1D(output(:,j).');
+%        output(:,j) = IDCTImpl(output(:,j).');
+%           output(:,j) = idct(output(:,j).');    
+    end
+end
+
+function output = blocDct2D(input, blkSize)
+
+    [row, col] = size(input);
+    output = zeros(row,col);
+    nCol = col / blkSize;
+    nRow = row/ blkSize;
+
+    for i = 1: nCol
+        for j = 1 : nRow
+            for a = 1: blkSize
+               output(a + blkSize*(j-1),(1+blkSize*(i-1)):(blkSize*i)) = dct1D(input(a + blkSize*(j-1),(1+blkSize*(i-1)):(blkSize*i)));
+            end
+            for b = 1: blkSize
+               output((1+blkSize*(j-1)):(blkSize*j),b + blkSize*(i-1)) = dct1D(output((1+blkSize*(j-1)):(blkSize*j),b + blkSize*(i-1)).');
+            end 
+        end
+    end
+end
+
+function output = blocInvDct2D(input, blkSize)
+
+    [row, col] = size(input);
+    output = zeros(row,col);
+    nCol = col / blkSize;
+    nRow = row/ blkSize;
+
+    for i = 1: nCol
+        for j = 1 : nRow
+            for a = 1: blkSize
+               output(a + blkSize*(j-1),(1+blkSize*(i-1)):(blkSize*i)) = invDct1D(input(a + blkSize*(j-1),(1+blkSize*(i-1)):(blkSize*i)));
+            end
+            for b = 1: blkSize
+               output((1+blkSize*(j-1)):(blkSize*j),b + blkSize*(i-1)) = invDct1D(output((1+blkSize*(j-1)):(blkSize*j),b + blkSize*(i-1)).');
+            end 
+        end
+    end
+end
+
+%% DHT
+function output = dht2D(input)
+    [row, col] = size(input);
+    output = zeros(row,col);
+    
+    for i = 1 : row
+        output(i,:) = ( HMatrix(row)*(input(i,:)).').' / row;
+    end
+    
+    for j = 1: col
+       output(:,j) = HMatrix(row) * output(:,j) / col;
+    end
+end
+
+function output = invDht2D(input)
+    [row, col] = size(input);
+    output = zeros(row,col);
+    
+    for i = 1 : row
+        output(i,:) = ( HMatrix(row)*input(i,:).').' * row;
+    end
+    
+    for j = 1: col
+       output(:,j) = HMatrix(row) * output(:,j) * col;
+    end
+end
+
+function output = HMatrix(inputsize)
+
+    H = [1 1 ; 1 -1];
+    N = log2(inputsize);
+    if N == 2
+        output = H;
+        return;
+    end
+    while N > 1
+        newMatrix = [H H; H -H];
+        H = newMatrix;
+        N = N-1;
+    end
+    output = H;
+end
+%% Wavelet Transform
+function [downLow, downHigh] = dwt1D(input, HV)
+    [row, col] = size(input);
+    lowOutput = zeros(row, col);
+    highOutput = zeros(row, col);
+    switch HV
+        case 'horizontal'
+            lowOutput(1,:) = (input(1,:) + input(row,:))/2;
+            highOutput(1,:) = (input(1,:) - input(row,:))/2;
+            for idx = 2 : row
+                lowOutput(idx,:) = (input(idx,:) + input(idx-1,:))/2;
+                highOutput(idx,:) = (input(idx,:) - input(idx-1,:))/2;
+            end
+
+            downLow = zeros(row/2,col);
+            downHigh = zeros(row/2,col);
+
+            for idx = 1:row/2
+               downLow(idx,:) = lowOutput(2*idx,:);
+               downHigh(idx,:) = highOutput(2*idx,:);
+            end
+            
+        case 'vertical'
+            lowOutput(:,1) = (input(:,1) + input(:,row))/2;
+            highOutput(:,1) = (input(:,1) - input(:,row))/2;
+            for idx = 2 : row
+                lowOutput(:,idx) = (input(:,idx) + input(:,idx-1))/2;
+                highOutput(:,idx) = (input(:,idx) - input(:,idx-1))/2;
+            end
+
+            downLow = zeros(row,col/2);
+            downHigh = zeros(row,col/2);
+
+            for idx = 1:row/2
+               downLow(:,idx) = lowOutput(:,2*idx);
+               downHigh(:,idx) = highOutput(:,2*idx);
+            end            
+    end
+end
+
+function ouput = invDwt1D(low, high, HV)
+    [row, col] = size(low);
+    
+    switch HV
+        case 'horizontal'
+            lowOutput = zeros(row*2, col);
+            highOutput = zeros(row*2, col);
+
+            for i = 1 : row
+                lowOutput = low(i,:);
+                highOutput = high(i,:);
+            end
+
+            [row, col] = size(lowOutput);
+            output = zeros(row,col);
+
+            output(1,:) = (lowOutput(1,:) + lowOutput(row,:)) + (highOutput(1,:) - highOutput(row,:));
+            for idx = 2 : row
+                output(idx,:) = (lowOutput(idx,:) + lowOutput(idx-1,:)) + (highOutput(idx,:) - highOutput(idx-1,:));
+
+            end
+    
+        case 'vertical'
+            lowOutput = zeros(row, col*2);
+            highOutput = zeros(row, col*2);
+
+            for i = 1 : col
+                lowOutput = low(:,i);
+                highOutput = high(:,i);
+            end
+
+            [row, col] = size(lowOutput);
+            output = zeros(row,col);
+
+            output(:,1) = (lowOutput(:,1) + lowOutput(:,row)) + (highOutput(:,1) - highOutput(:,row));
+            for idx = 2 : col
+                output(:,idx) = (lowOutput(:,idx) + lowOutput(:,idx-1)) + (highOutput(:,idx) - highOutput(:,idx-1));
+
+            end
+    end
+    
 end
--- HW/Assignment_4/Problem_2.m
+++ HW/Assignment_4/Problem_2.m
@@ -1,5 +1,396 @@
 function Problem_2()
 
+    %% loading directory for image files
+    imgdir= uigetdir('Image Directory');
+    file = fopen(fullfile(imgdir,'\Gray_baby_512x512.raw'),'rb');
+    gray_image = fread(file,fliplr([512,512]),'*uint8')';
+    fclose(file);
+   
+    file = fopen(fullfile(imgdir,'\Color_baby_512x512.raw'),'rb');
+    color_image = fread(file,fliplr([512,512*3]),'*uint8')';
+    fclose(file);   
+    %% -------------- insert code below --------------%%
+
+    
+    grayHistEq = histEqualizer(gray_image);
+    figure, imshow(grayHistEq,[]), title('2.1 a Histogram equalization Gray Image')
+    colorHistEq = histEqualizerColor(color_image);
+    figure, imshow(colorHistEq,[]), title('2.1 a Histogram equalization RGB Image')
+%     hsiHistEq = histEqualizerHSI(color_image)
+%     figure, imshow(hsiHistEq,[]), title('2.1 a Histogram equalization HSI Image')
+% 
+%     figure, imshow(colorHistEq,[]), title('2.1 a Histogram equalization CMY Image')
+
+    alpha = 0.5;
+    grayHistModi = histModification(gray_image, alpha);
+    figure, imshow(grayHistModi, []), title('2.1 b Histogram Modification Gray Image')
+    
+    gamma = 2;
+    gammaCorrection(color_image, gamma);
+    figure, imshow(grayHistModi, []), title('2.2 a Gamma Correction Color Image')
+    
+    grayguasNois = guasNoise(gray_image);
+    graysaltNois = saltpepperNoise(gray_image);
+    figure, imshow(grayguasNois, []), title('2.2 b image with gaussian noise')
+    figure, imshow(graysaltNois, []), title('2.2 b image with salt pepper noise')
+    
+    meansalt3 = meanfilter(graysaltNois, 3);
+    meansalt7 = meanfilter(graysaltNois, 7);
+    meangaus3 = meanfilter(grayguasNois, 3);
+    meangaus7 = meanfilter(grayguasNois, 7);
+    figure, imshow(meansalt3, []), title('2.2 b Mean filtered Salt image (3x3 kernel)')
+    figure, imshow(meansalt7, []), title('2.2 b Mean filtered Saltimage (7x7 kernel)')    
+    figure, imshow(meangaus3, []), title('2.2 b Mean filtered Guassian image (3x3 kernel)')
+    figure, imshow(meangaus7, []), title('2.2 b Mean filtered Guassian image (7x7 kernel)')    
+    
+    mediansalt3 = medianfilter(graysaltNois, 3);
+    mediansalt7 = medianfilter(graysaltNois, 7);
+    mediangaus3 = medianfilter(grayguasNois, 3);
+    mediangaus7 = medianfilter(grayguasNois, 7);
+    figure, imshow(mediansalt3, []), title('2.2 b Median filtered Salt image (3x3 kernel)')
+    figure, imshow(mediansalt7, []), title('2.2 b Median filtered Salt image (7x7 kernel)')    
+    figure, imshow(mediangaus3, []), title('2.2 b Median filtered Guassian image (3x3 kernel)')
+    figure, imshow(mediangaus7, []), title('2.2 b Median filtered Guassian image (7x7 kernel)')        
+end
+%% histogram equalization 
+function output = histEqualizer(input)
+    [row, col] = size(input);
+    
+    pixelTotal = row*col;   
+    % compute pdf
+    pdf = zeros(256,1);
+    for i = 1: row
+        for j = 1: col
+            val = input(i,j);
+            pdf(val+1) = pdf(val+1) + 1;
+        end
+    end
+    pdf = pdf/ pixelTotal;
+    
+    % compute cdf
+    cdf= zeros(256,1);
+    cdf(1) = pdf(1);
+    for i = 2: size(pdf,1)
+        cdf(i) = pdf(i) + cdf(i-1);
+
+    end
+    
+        normalizedCDF = round ((cdf - min(cdf)) / (1 - min(cdf)) * 255 + 0.5);
+    output = size(input);
+    for i = 1:row
+        for j = 1:col
+            output(i,j) = normalizedCDF(input(i,j)+1);
+        end  
+    end
+    
+
+end
+
+function output = histEqualizerColor(input)
+    [row, col] = size(input);
+    
+    % seperate RGB
+    R = zeros(row, col/3);
+    G = zeros(row, col/3);
+    B = zeros(row, col/3);
+    
+    for i = 1 : col/3
+        for j = 1 : row
+            R(j,i) = input(j,3*(i-1) + 1);
+            G(j,i) = input(j,3*(i-1) + 2);
+            B(j,i) = input(j,3*(i-1) + 3);
+        end
+    end
+    
+    outputR = histEqualizer(R);
+    outputG = histEqualizer(G);
+    outputB =histEqualizer(B);
+    
+    output = zeros(row, col/3 , 3);
+    
+    output(:,:,1) =outputR;
+    output(:,:,2) =outputG;
+    output(:,:,3) =outputB;
+    
+    output = output / 255;
+    
+    output1 = zeros(row, col/3 , 3);
+    
+    output1(:,:,1) =R;
+    output1(:,:,2) =G;
+    output1(:,:,3) =B;
+    
+    output1 = output1/255;
+    figure, imshow(output, [])
+    figure, imshow(output1, [])
+end
+
+function output = histEqualizerHSI(input)
+    [row, col] = size(input);
+    
+    % seperate RGB
+    R = zeros(row, col/3);
+    G = zeros(row, col/3);
+    B = zeros(row, col/3);
+    
+    for i = 1 : col/3
+        for j = 1 : row
+            R(j,i) = input(j,3*(i-1) + 1);
+            G(j,i) = input(j,3*(i-1) + 2);
+            B(j,i) = input(j,3*(i-1) + 3);
+        end
+    end
+    % convert to HSI
+    [H S I]= rgb2HSI( R,G,B )
+    outputH = histEqualizer(floor(H*255))/255;
+    outputS = histEqualizer(floor((S+0.5)*255))/255-0.5;
+    outputI =histEqualizer(floor(I*255))/255;
+    
+   [newR newG newB ] = HSItoRGB( outputH, outputS, outputI )   
+    
+    output = zeros(row, col/3 , 3);
+    
+    output(:,:,1) =floor(newR);
+    output(:,:,2) =floor(newG);
+    output(:,:,3) =floor(newB);
+    
+    output = output / 255;
+    
+    figure, imshow(output, [])
+end
+
+% HSI RGB conversion
+function [ H, S, I ] = rgb2HSI( R,G,B )
+
+    I = 1/3 .*(R+G+B);
+
+    S=1- (3./(R+G+B)+0.000001).*(min(min(R,G),B));
+
+    H = acosd ((2*R-G-B) ./(2*sqrt( ( (R-G).^2 + (R-B).*(G-B) ) )+0.000001)); % 1/(2*pi)
+
+    H(B>G)=360-H(B>G);
+    H=H/360;
+end
+
+function [ newR newG newB ] = HSItoRGB( H, S, I )
+
+
+
+    Chroma = S .* I;
+    Hdash = H*360.0 / 60.0;
+    X = Chroma .* (ones(size(Hdash)) - abs(rem(Hdash,2.0) -ones(size(Hdash))));
+    Min = I - Chroma;
+
+    newR = zeros(size(H));
+    newG = zeros(size(H));
+    newB = zeros(size(H));
+
+    newR = newR +(Hdash < 1.0).*Chroma ; 
+    newG = newG +(Hdash < 1.0).*X ;
+
+    newR = newR +(Hdash>= 1.0 & Hdash < 2.0).*X ; 
+    newG = newG +(Hdash>= 1.0 & Hdash< 2.0).*Chroma ;
+
+    newG = newG +(Hdash >= 2.0 & Hdash < 3.0).*Chroma ; 
+    newB = newB +(Hdash >= 2.0 & Hdash < 3.0).*X ;
+
+    newG = newG +(Hdash >= 3.0 & Hdash < 4.0).*X ; 
+    newB = newB +(Hdash >= 3.0 & Hdash < 4.0).*Chroma ;
+
+    newR = newR +(Hdash >= 4.0 & Hdash < 5.0).*X ; 
+    newB = newB +(Hdash >= 4.0 & Hdash < 5.0).*Chroma ;
+
+    newR = newR+(Hdash >= 5.0 & Hdash < 6.0).*Chroma ; 
+    newB = newB +(Hdash >= 5.0 & Hdash < 6.0).*X ;
+
+    newR = newR + Min;
+    newG = newG + Min;
+    newB = newB + Min;
+
+end
+
+
+%% histogram modification
+
+function output = histModification(input, alpha)
+    [row, col] = size(input);
+    
+    pixelTotal = row*col;   
+    % compute pdf
+    pdf = zeros(256,1);
+    for i = 1: row
+        for j = 1: col
+            val = input(i,j);
+            pdf(val+1) = pdf(val+1) + 1;
+        end
+    end
+    pdf = pdf/ pixelTotal;
+    
+    % compute cdf
+    cdf= zeros(256,1);
+    cdf(1) = pdf(1);
+    for i = 2: size(pdf,1)
+        cdf(i) = pdf(i) + cdf(i-1);
+    end
+    
+    normalizedCDF =  round((1 - exp(-(cdf.^2 / (2*alpha^2))))*255);
+    output = size(input);
+    for i = 1:row
+        for j = 1:col
+            output(i,j) = normalizedCDF(input(i,j)+1);
+        end  
+    end
+    
+end
+
+
+%% gamma correction
+
+function output = gammaCorrection(input, gamma)
+
+    gammaCorrection = 1/ gamma;
+    
+    [row, col] = size(input);
+    
+    % seperate RGB
+    R = zeros(row, col/3);
+    G = zeros(row, col/3);
+    B = zeros(row, col/3);
+    
+    for i = 1 : col/3
+        for j = 1 : row
+            R(j,i) = input(j,3*(i-1) + 1);
+            G(j,i) = input(j,3*(i-1) + 2);
+            B(j,i) = input(j,3*(i-1) + 3);
+        end
+    end
+    
+    
+    newR = 255*(R./255).^gammaCorrection;
+    newG = 255*(G./255).^gammaCorrection;
+    newB = 255*(B./255).^gammaCorrection;
+
+    
+   output = zeros(row, col/3 , 3);
+    
+    output(:,:,1) =newR;
+    output(:,:,2) =newG;
+    output(:,:,3) =newB;
+    
+    output = output./255;
+    figure, imshow(output, [])
+
+end
+
+
+
+%% Noise
+
+function output = guasNoise(input)
+  mean = 0;     % default mean
+  variance = 0.01;  % default variance
+  output = double(input) + sqrt(variance)*randn(size(input));  
+end
+
+function output = saltpepperNoise(input)
+    d = 0.01;   % default density
+    output = input;
+    x = rand(size(input));
+    output(x < d/2) = 0; % Minimum value
+    output(x >= d/2 & x < d) = 1; % Maximum (saturated) value
+    
+    
+end
+
+%% mean filter
+function output = meanfilter(input, kernelSize)
+    matrixSize = kernelSize^2;
+    kernel = ones(kernelSize, kernelSize) ./ matrixSize;
+    output = colvolution(input,kernel);
+
+end
+
+
+function output = colvolution(input, kernel)
+    % invert
+    [row, col] = size(kernel);
+    temp = zeros(row,col);
+    for i = 1: row
+        for j = 1: col
+           temp(i,j) = kernel(row - i +1, col - j +1);
+        end 
+    end
+    kernel = temp;
+    
+    [row1, col1] = size(input);
+    a  = ceil(row /2);
+    output = zeros(row1, col1);
+    for i = 1 :row1
+        for j = 1: col1
+            for k = 1:row
+                for l = 1:col
+                    if (i-a+k >0) &  (i-a+k <= row1) &  (j-a+l >0) & (j-a+l <= col1)
+                        output(i,j) = output(i,j) + input(i-a+k,j-a+l) * kernel(k,l);
+                    end
+                end
+            end
+            
+        end 
+    end
+     
 
 
 end
+%% median filter
+
+function output = medianfilter(input, kernelSize)
+    matrixSize = kernelSize^2;
+    kernel = ones(kernelSize, kernelSize);
+    output = colvolution4median(input,kernel);
+
+end
+
+function output = colvolution4median(input, kernel)
+    % invert
+    [row, col] = size(kernel);
+    temp = zeros(row,col);
+    for i = 1: row
+        for j = 1: col
+           temp(i,j) = kernel(row - i +1, col - j +1);
+        end 
+    end
+    kernel = temp;
+    
+    [row1, col1] = size(input);
+    a  = ceil(row /2);
+    temp = zeros(row*col,1);
+    output = zeros(row1, col1);
+    for i = 1 :row1
+        for j = 1: col1
+            
+            for k = 1:row
+                for l = 1:col
+                    if (i-a+k >0) &  (i-a+k <= row1) &  (j-a+l >0) & (j-a+l <= col1)
+                        temp((k-1)*col+l,1) =  input(i-a+k,j-a+l);
+                    else
+                        notsort = 1;
+                    end
+                end
+            end
+            
+            
+            if notsort == 1
+                output(i,j) = input(i,j);
+            else
+                sortMatrix = sort(temp);
+                output(i,j)= sortMatrix(a);
+                              
+            end
+            notsort = 0 ;
+            temp = zeros(row*col,1);  
+
+            
+        end 
+    end
+ end
+%% directional filite
