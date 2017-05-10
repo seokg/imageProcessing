8f00a895a4821cf30a8cead0049c2696777f6903
Edward Seo <seokg1023@gmail.com>
2017-05-11 00:16:21 +0900

    Add files via upload

--- HW/Assignment_5/Problem_1.m
+++ HW/Assignment_5/Problem_1.m
@@ -19,19 +19,12 @@ function Problem_1()
 %%  1.1 
 
 % % hamming window / blur image
-% % Outer Product:
-% % --------------
-% w1 = hamming(15);
-% w2 = w1(:) * w1(:).';
-
-% % Rotational
-% % ----------
-% w1 = hamming(15);
-% [x,y] = meshgrid(-7:7);
-% r = sqrt(x.^2 + y.^2);
-% w = zeros(size(r));
-% w(r<=7) = interp1(linspace(-7,7,15),w1,r(r<=7));
-
+    window = hammingWindow(size(grayLenna,1));
+    blurImage = window.*dft2D(double(grayLenna));
+    magnitude = log(1+abs(blurImage));
+    figure; imshow(magnitude,[]); title('blur image');
+    
+    
 % adding gaussian nosie
 %     matlabNosie = imnoise(grayLenna,'gaussian');
 %     figure; imshow(matlabNosie,[]); title('matlab noise');
@@ -53,6 +46,117 @@ function output = hammingWindow(inputSize)
         w(idx) = 0.54 - 0.46*cos(2 * pi * (idx-1)/(inputSize-1));
     end
     
-    output = w* w.';
+    output = w.'* w;
 
+end
+
+
+%% DFT
+function output = dft1D(input)
+    %% functions code
+    [~ , col] = size(input);
+    output = zeros(1,col);
+    if(col==1)
+       output = input(col) ;
+    else
+        idxOdd = 1;
+        idxEven = 1;
+        odd = zeros(1, col/2);
+        even = zeros(1,col/2);
+
+        for i = 1 : col
+            checkOddEven = mod(i,2);
+            if (checkOddEven==1)
+                odd(idxOdd) = input(i);
+                idxOdd = idxOdd + 1;
+            else
+                even(idxEven) = input(i);
+                idxEven = idxEven + 1;
+            end 
+        end
+        evenOut = dft1D(odd); % start with 0 in equation 
+        oddOut = dft1D(even); % start with 1 in equation
+        
+        for j = 1: col/2
+           expVal = - 1i * 2 * pi * (j-1) / col;
+           output(j)= evenOut(j) + oddOut(j) * exp (expVal);
+           output(j + col/2) = evenOut(j) - oddOut(j) * exp (expVal);
+           
+        end
+        
+    end
+
+end
+
+function  output = dft2D(input)
+    [row, col] = size(input);
+    output = zeros(row,col);
+    
+
+    for i = 1 : row
+        output(i,:) = dft1D(input(i,:));
+    end  
+    
+    for j = 1: col
+       output(:,j) = dft1D(output(:,j).');
+    end
+
+
+end
+
+function output = invDft1D(input)
+
+      %% functions code
+    [~ , col] = size(input);
+    output = zeros(1,col);
+    if(col==1)
+       output = input(col) ;
+
+    else
+        idxOdd = 1;
+        idxEven = 1;
+        odd = zeros(1, col/2);
+        even = zeros(1,col/2);
+
+        for i = 1 : col
+            checkOddEven = mod(i,2);
+            if (checkOddEven==1)
+                odd(idxOdd) = input(i);
+                idxOdd = idxOdd + 1;
+            else
+                even(idxEven) = input(i);
+                idxEven = idxEven + 1;
+            end 
+        end
+%         odd = input(2:2:col);
+%         even = input(1:2:col);
+%         evenOut = invDft1D(even);
+%         oddOut = invDft1D(odd);
+        evenOut = invDft1D(odd);
+        oddOut = invDft1D(even);        
+        for j = 1: col/2
+           expVal = 1i * 2 * pi * (j-1) / col;
+           output(j)= evenOut(j) + oddOut(j) * exp (expVal);
+           output(j + col/2) = (evenOut(j) - oddOut(j) * exp (expVal));
+           
+        end
+        output = output/2;
+    end
+    
+end
+
+function output = invDft2D(input)
+
+    [row, col] = size(input);
+    output = zeros(row,col);
+    
+    for i = 1 : row
+        output(i,:) = invDft1D(input(i,:));
+    end
+    
+    for j = 1: col
+       output(:,j) = invDft1D(output(:,j).');
+    end
+    
+%     output = 1/(row*col) * output;
 end
